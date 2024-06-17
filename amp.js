6f84f8c6baa87086f600a435f0b78f8131e16e6e
haverfordismlab <haverfordismlab@gmail.com>
2024-06-17 09:04:17 -0400

    Update amp.js

--- amp.js
+++ amp.js
@@ -261,7 +261,9 @@ define(['pipAPI','underscore'], function(APIConstructor, _) {
 		});
 
 		var rightSortingLabel = piCurrent.sortingLabel1; 
-		var leftSortingLabel = piCurrent.sortingLabel2; 
+		var leftSortingLabel = piCurrent.sortingLabel2;
+		var maxW = piCurrent.touchMaxStimulusWidth;
+		var maxH = piCurrent.touchMaxStimulusHeight;
 		if (piCurrent.randomizeLabelSides && Math.random() < 0.5)
 		{
 			rightSortingLabel = piCurrent.sortingLabel2;
